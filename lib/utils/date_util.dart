c3851a79b474fccfb39811761e801717915654eb
Chansy KHUEXONLUE <www.ainythor@gmail.com>
2023-07-31 21:09:30 +0700

    Fix get post

--- lib/utils/date_util.dart
+++ lib/utils/date_util.dart
@@ -1,8 +1,15 @@
+import 'package:intl/intl.dart';
+
 String formateDate(String date) {
   DateTime dateTime = DateTime.parse(date);
   DateTime now = DateTime.now();
   Duration duration = now.difference(dateTime);
-  if (duration.inDays > 0) {
+
+  if (duration.inDays >= 7) {
+    // Format the date to "Month day, year" (e.g., "July 12, 2023")
+    String formattedDate = DateFormat('MMMM d, y').format(dateTime);
+    return formattedDate;
+  } else if (duration.inDays > 0) {
     return "${duration.inDays} days ago";
   } else if (duration.inHours > 0) {
     return "${duration.inHours} hours ago";
